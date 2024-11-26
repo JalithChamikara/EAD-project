b8bfa89ee8af6284f56eb91bb95fb49515cdfb26
sasmitha27 <sasmitha.vishvadinu27@gmail.com>
2024-11-26 20:05:26 +0530

    Footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,43 +6,43 @@ const Footer = () => {
             <div className="fLists">
                 <ul className="fList">
                     <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
                 </ul>
                 <ul className="fList">
                     <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
                 </ul>
                 <ul className="fList">
                     <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
                 </ul>
                 <ul className="fList">
                     <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
                 </ul>
                 <ul className="fList">
                     <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
                 </ul>
             </div>
            <div className="ftext">Copyright 2024</div>
