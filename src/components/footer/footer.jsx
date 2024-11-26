48c2f65952e51138658c882cb8a96ce90617e7b7
sasmitha27 <sasmitha.vishvadinu27@gmail.com>
2024-11-26 20:24:07 +0530

    icon packages

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,16 +1,23 @@
 import "./footer.css"
+import { FaGlobe, FaCity, FaMap, FaPlane, FaHotel } from "react-icons/fa";
 
 const Footer = () => {
     return(
         <div className="footer">
             <div className="fLists">
                 <ul className="fList">
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Regions</li>
-                    <li className="fListItem">Cities</li>
-                    <li className="fListItem">Districts</li>
-                    <li className="fListItem">Airports</li>
-                    <li className="fListItem">Hotels</li>
+                    <li className="fListItem">
+                    <FaGlobe className="footer-icon" />Countries</li>
+                    <li className="fListItem">
+                    <FaMap className="footer-icon" />Regions</li>
+                    <li className="fListItem">
+                    <FaCity className="footer-icon" />Cities</li>
+                    <li className="fListItem">
+                    <FaMap className="footer-icon" />Districts</li>
+                    <li className="fListItem">
+                    <FaPlane className="footer-icon" />Airports</li>
+                    <li className="fListItem">
+                    <FaHotel className="footer-icon" />Hotels</li>
                 </ul>
                 <ul className="fList">
                     <li className="fListItem">Countries</li>
